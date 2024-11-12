8abbacb153e6dc6d09757180396209936d7fda24
Deise Domingues <dominguesdeise@gmail.com>
2024-11-11 21:05:18 -0300

    feat: adiciona script da criação do banco de dados e tabelas

---
+++ TCC_DEGI-main/index.js
@@ -0,0 +1,10 @@
+console.log("Hello, NodeJS!")
+
+const express = require ('express')
+const app = express()
+app.use(express.json())
+//GET http://localhost:3000/hey
+app.get('/hey', (req, res) => {
+res.send('hey')
+})
+app.listen(3000, () => console.log("up and running"))

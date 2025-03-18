0a4eafd54280daa75a767ed1c1c2c79a2a1264a0
Giovanna Fonseca <giovannafonsecasilva2004@gmail.com>
2025-03-18 08:45:53 -0300

    feat: implementa requisição POST para login

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,9 @@ app.listen(3000, () => console.log("Up and running"));
 
 app.post('/register', async (req, res) => {
     try {
-        const { login, password } = req.body;
+        const { email, password } = req.body;
 
-        if (!login || !password) {
+        if (!email || !password) {
             return res.status(400).json({ message: 'Login e senha são obrigatórios!' });
         }
 
@@ -26,7 +26,7 @@ app.post('/register', async (req, res) => {
             return res.status(400).json({ message: 'Sua senha deve ter pelo menos 6 caracteres.' });
         }
 
-        if (usersbd[login]) {
+        if (usersbd[email]) {
             return res.status(409).json({ message: 'Usuário já existe!' });
         }
 
@@ -34,13 +34,34 @@ app.post('/register', async (req, res) => {
 
         console.log(`Hash gerado para a senha: ${cript}`);
 
-        usersbd[login] = { login, password: cript };
+        usersbd[email] = { email, password: cript };
 
-        console.log(`Usuário ${login} cadastrado com sucesso!`);
+        console.log(`Usuário ${email} cadastrado com sucesso!`);
         res.status(201).json({ message: 'Usuário cadastrado com sucesso!' });
 
     } catch (error) {
         console.log('Erro ao tentar cadastrar usuário:', error);
         res.status(500).json({ message: 'Erro interno no servidor' });
+    }   
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const user = usersbd[email];
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Login e senha são obrigatórios!' });
     }
+
+    if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+        return res.status(401).json({ message: 'Senha incorreta' });
+    }
+
+    res.status(200).json({ message: 'Login bem-sucedido' });
 });

ef1799e496cbfecca00925ea5c8bc06283c4354c
Giovanna Fonseca <giovannafonsecasilva2004@gmail.com>
2025-06-23 13:24:03 -0300

    style: redesign da tela de login

--- frontend/src/app/auth/login.js
+++ frontend/src/app/auth/login.js
@@ -1,181 +1,187 @@
 import { router, useLocalSearchParams } from "expo-router";
-import { ScrollView, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity } from "react-native";
-import * as Animatable from "react-native-animatable"; // animação
-import axios from 'axios'; // para requisições HTTP
-import { useState } from 'react';
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+import axios from "axios";
+import { useState } from "react";
+import { FontAwesome } from "@expo/vector-icons";
 import api from "../api/api";
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Login() {
   const { name } = useLocalSearchParams();
 
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [loading, setLoading] = useState(false); 
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [erro, setErro] = useState("");
 
-  // Direcionar para a tela de recuperação de senha
   const recuperarSenha = () => {
     router.push("/auth/recuperar-senha");
   };
 
-  // Voltar para a tela anterior
   function voltar() {
     router.back();
   }
 
-  // Envia os dados do login para o backend
   const enviaLogin = async () => {
     if (!email || !senha) {
-      alert("Por favor, preencha todos os campos.");
+      setErro("Por favor, preencha todos os campos.");
       return;
     }
+
+    setErro("");
 
     const emailValido = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$/;
 
     if (!emailValido.test(email)) {
-      alert('Por favor, insira um email válido.');
+      alert("Por favor, insira um email válido.");
       return;
     }
 
-    setLoading(true); 
+    setLoading(true);
 
     try {
-      const response = await api.post('/login', {
-        email,
-        senha 
-      });
-
+      const response = await api.post("/login", { email, senha });
       console.log(response.data);
       router.push("/telas/home");
-
     } catch (error) {
       console.error(error);
-      alert('Erro no login. Verifique seus dados e tente novamente.');
+      alert("Erro no login. Verifique seus dados e tente novamente.");
     } finally {
-      setLoading(false); 
+      setLoading(false);
     }
   };
 
   return (
-    <SafeAreaView style={styles.telaLogin}>
-      {/* Animação do cabeçalho */}
-      <Animatable.View
-        animation="fadeInLeft"
-        delay={500}
-        style={styles.cabecalho}
-      >
-        <Text style={styles.mensagemBoasVindas}>Bem-vindo(a)!</Text>
-      </Animatable.View>
+    <LinearGradient colors={["#1E1E1E", "#473CA6", "#2F253E"]} style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Animatable.View animation="fadeInLeft" delay={500} style={styles.cabecalho}>
+          <Text style={styles.logo}>Musician&Match</Text>
+        </Animatable.View>
 
-      {/* Formulário animado */}
-      <Animatable.View
-        animation="fadeInUp"
-        style={styles.areaFormulario}
-      >
-        {/* Campo de Email */}
-        <Text style={styles.tituloCampo}>Email</Text>
-        <TextInput
-          placeholder="Digite seu email"
-          style={styles.campoTexto}
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-        />
+        <Animatable.View animation="fadeInUp" style={styles.areaFormulario}>
+          <View style={styles.inputContainer}>
+            <FontAwesome name={"at"} size={30} color={"#ffff"} style={styles.inputIcon} />
+            <TextInput
+              style={styles.textInput}
+              placeholder="E-mail"
+              placeholderTextColor="#FFFFFF80"
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+            />
+          </View>
 
-        {/* Campo de Senha */}
-        <Text style={styles.tituloCampo}>Senha</Text>
-        <TextInput
-          placeholder="Digite sua senha"
-          style={styles.campoTexto}
-          secureTextEntry
-          value={senha}
-          onChangeText={setSenha}
-        />
+          <View style={styles.inputContainer}>
+            <FontAwesome name={"lock"} size={30} color={"#ffff"} style={styles.inputIcon} />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Senha"
+              value={senha}
+              placeholderTextColor="rgba(255, 255, 255, 0.5)"
+              onChangeText={setSenha}
+              secureTextEntry
+            />
+          </View>
 
-        {/* Link para recuperar senha */}
-        <Text style={styles.textoEsqueceuSenha} onPress={recuperarSenha}>
-          Esqueceu sua senha?
-        </Text>
-
-        {/* Botão "Acessar" */}
-        <TouchableOpacity
-          style={styles.botaoLogin}
-          onPress={enviaLogin}
-        >
-          <Text style={styles.textoBotaoLogin}>
-            {loading ? 'Acessando...' : 'Acessar'}
+          <Text style={styles.textoEsqueceuSenha} onPress={recuperarSenha}>
+            Esqueceu sua senha?
           </Text>
-        </TouchableOpacity>
 
-        {/* Botão "Cadastre-se" */}
-        <TouchableOpacity
-          style={styles.botaoCadastro}
-          onPress={() => router.push("/auth/register")}
-        >
-          <Text style={styles.textoCadastro}>
-            Não possui uma conta? <Text style={styles.textoCadastreSe}>Cadastre-se</Text>
-          </Text>
-        </TouchableOpacity>
-      </Animatable.View>
+          {erro !== "" && (
+            <Text style={{ color: "red", textAlign: "center", marginBottom: 10 }}>{erro}</Text>
+          )}
 
-      {/* Botão "Voltar" */}
-      <TouchableOpacity
-        style={styles.botaoVoltar}
-        onPress={voltar}
-      >
-        <Text>{name}</Text>
-        <Text style={styles.textoBotaoVoltar}>Voltar</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
+          <TouchableOpacity style={styles.botaoLogin} onPress={enviaLogin}>
+            <Text style={styles.textoBotaoLogin}>{loading ? "Acessando..." : "Acessar"}</Text>
+          </TouchableOpacity>
+
+          <View style={styles.divisorContainer}>
+            <View style={styles.bolinha} />
+            <View style={styles.linha} />
+            <View style={styles.bolinha} />
+          </View>
+
+          <TouchableOpacity style={styles.botaoGoogle}>
+            <FontAwesome name="google" size={30} color="#fff" />
+            <Text style={styles.textoBotaoGoogle}>Acessar com Google</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botaoCadastro} onPress={() => router.push("/auth/register")}>
+            <Text style={styles.textoCadastro}>
+              Não possui uma conta? <Text style={styles.textoCadastreSe}>Cadastre-se</Text>
+            </Text>
+          </TouchableOpacity>
+        </Animatable.View>
+
+        <TouchableOpacity style={styles.botaoVoltar} onPress={voltar}>
+          <Text>{name}</Text>
+          <Text style={styles.textoBotaoVoltar}>Voltar</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
-// Styles
 const styles = StyleSheet.create({
-  telaLogin: {
-    flex: 1,
-    backgroundColor: "#871F78",
-  },
   cabecalho: {
     marginTop: "14%",
     marginBottom: "8%",
-    paddingStart: "5%",
   },
-  mensagemBoasVindas: {
-    fontSize: 28,
+  logo: {
+    fontSize: 40,
     fontWeight: "bold",
     color: "#fff",
+    textAlign: "center",
+    marginBottom: 50,
   },
   areaFormulario: {
-    backgroundColor: "#fff",
-    flex: 1,
+    flex: 2,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    paddingStart: "5%",
-    paddingEnd: "5%",
+    paddingStart: "8%",
+    paddingEnd: "8%",
+    paddingHorizontal: "8%",
   },
-  tituloCampo: {
-    fontSize: 20,
-    marginTop: 25,
-  },
-  campoTexto: {
-    borderBottomWidth: 1,
-    height: 40,
-    marginBottom: 12,
-    fontSize: 16,
-  },
-  botaoLogin: {
-    backgroundColor: "#871F78",
+  botaoLogin: { //Acessar
+    backgroundColor: "#fff",
     width: "100%",
-    borderRadius: 10,
-    paddingVertical: 8,
+    borderRadius: 25,
+    paddingVertical: 15,
     marginTop: 14,
     justifyContent: "center",
     alignItems: "center",
+    elevation: 5,
   },
   textoBotaoLogin: {
+    color: "#463BA2",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  botaoGoogle: {
+    backgroundColor: "transparent",
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 25,
+    borderColor: "#fff",
+    paddingVertical: 15,
+    marginTop: 14,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row", 
+  },
+  textoBotaoGoogle: {
     color: "#fff",
     fontSize: 18,
     fontWeight: "bold",
+    marginLeft: 60, 
   },
   botaoVoltar: {
     backgroundColor: "#fff",
@@ -187,7 +193,7 @@ const styles = StyleSheet.create({
     marginLeft: 14,
     justifyContent: "center",
     alignItems: "center",
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   textoBotaoVoltar: {
     color: "#871F78",
@@ -204,11 +210,56 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   textoCadastreSe: {
-    color: "#871F78",
+    color: "#fff",
   },
   textoEsqueceuSenha: {
     color: "#c3c3c3",
     fontSize: 14,
     fontWeight: "bold",
-  }
+    marginStart: "8%",
+  },
+  inputContainer: {
+    backgroundColor: "rgba(38, 0, 0, 0.2)",
+    flexDirection: "row",
+    borderRadius: 25,
+    marginVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: "center",
+    height: 60,
+    elevation: 65,
+    width: "100%",
+
+    //ios
+    shadowColor: "#000",              
+    shadowOffset: { width: 0, height: 4 },  
+    shadowOpacity: 0.3,               
+    shadowRadius: 6,
+  },
+  inputIcon: {
+    marginLeft: 15,
+  },
+  textInput: {
+    flex: 1,
+    paddingLeft: 15
+  },
+  bolinha: {
+  width: 10,
+  height: 10,
+  borderRadius: 5,
+  backgroundColor: "#fff",
+  },
+  linha: {
+    flex: 1,
+    height: 1,
+    backgroundColor: "#fff",
+    opacity: 0.4,
+  },
+  divisorContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    marginVertical: 20,
+  },
 });
+
